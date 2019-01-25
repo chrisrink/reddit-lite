52e61b722989114fffe92678cd09e54147d1f1a2
Chris Rink <chrismrink@yahoo.com>
2019-01-25 02:19:22 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a3901d031336cf76d1b8e0a7f37046e.js"
+  "/precache-manifest.7553b139968913c0ca22ccdaa4691f0d.js"
 );
 
 workbox.clientsClaim();
